c723830f2e6a6e526d9cefa040cc810684d37223
TheActualBest <jcarfang@gmail.com>
2016-03-07 13:01:51 -0500

    Added files via upload

--- calculator.js
+++ calculator.js
@@ -1,28 +1,29 @@
-// Calculator Program
-// 4 Operations addition, subtraction,
-// multiplication, and division
-// You will need a function for each one
-// You need a menu so user can select an
-// operation
-// input
-
-//var choice = prompt("What operation", "addition");
 var handleClick = function(event){
 	console.log(window.event);
 	var choice = window.event.target.id;
+	var number1 = document.getElementById("firstnum").value;
+	var number2 = document.getElementById("secondnum").value;
+	var parsednumber1 = parseFloat(number1);
+	var parsednumber2 = parseFloat(number2);
+	var answer;
+	var operation;
 
-		var number1 = document.getElementById("firstnum").value;
-		var number2 = document.getElementById("secondnum").value;
-		var parsednumber1 = parseFloat(number1);
-		var parsednumber2 = parseFloat(number2);
 	if(choice == "addition"){
-		var answer = parsednumber1 + parsednumber2;
-	}else if(choice == "subtraction"){
-		var answer = parsednumber1 - parsednumber2;	
-	}else if(choice == "multiplication"){
-		var answer = parsednumber1 * parsednumber2;
-	}else if(choice == "division"){
-		var answer = parsednumber1 / parsednumber2;
-	}document.write("the answer is " + answer);
-		console.log("the answer is " + answer);
+		operation = " + ";
+	 answer = parsednumber1 + parsednumber2;		
+	} else if(choice == "subtraction"){
+		operation = " - ";
+	answer = parsednumber1 - parsednumber2;		
+	} else if(choice == "multiplication"){
+		operation = " * ";
+	 answer = parsednumber1 * parsednumber2;
+	} else if(choice == "division"){
+		operation = " / ";
+	 answer = parsednumber1 / parsednumber2;
+	}
+	
+	var answersArea = document.getElementById("answers");
+	var answerOutput;
+	answersArea.innerHTML = number1 + operation + number2 + " = " + answer;
+	
 }
